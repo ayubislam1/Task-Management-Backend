fd76ab1bfba46d1f9de0b58b7f7c724f089c4564
Ayub Islam <ayubislam863@gmail.com>
2025-02-21 04:17:13 +0600

    task post api create

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"] }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ds3da.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -21,7 +20,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
 	try {
-		// Connect the client to the server	(optional starting in v4.7)
+		const UserCollection = client.db("Task_Users").collection("all-users");
+		const TaskCollection = client.db("Task_Users").collection("tasks");
+		app.post("/all-users", async (req, res) => {
+			const users = req.body;
+			const query = { email: users.email };
+
+			const existingUser = await UserCollection.findOne(query);
+
+			if (existingUser) {
+				return res.send({ message: "User already exist" });
+			}
+
+			const result = await UserCollection.insertOne(users);
+			res.send(result);
+		});
+
+		app.post("/tasks", async (req, res) => {
+			const task = req.body;
+			const result = await TaskCollection.insertOne(task);
+			res.send(result);
+		});
 		await client.connect();
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
@@ -30,7 +49,7 @@ async function run() {
 		);
 	} finally {
 		// Ensures that the client will close when you finish/error
-		await client.close();
+		// await client.close();
 	}
 }
 run().catch(console.dir);
